dce81922af9e2eec556f74127013c56d7879f059
Michael Townsend <michael@doublespeakgames.com>
2021-10-12 22:03:19 -0400

    Fixed server routing in prod

--- app/server.mjs
+++ app/server.mjs
@@ -7,7 +7,12 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.static("dist"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(port, () => console.log(`App hosted on port ${port}`));
